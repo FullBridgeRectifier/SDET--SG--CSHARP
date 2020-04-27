4d1f88e79fcbd374b9a531876fc63e28915626d1
Victor Moran <VMoran@dealersocket.com>
2020-04-27 10:22:24 -0500

    added searchpage methods and locators

--- aliexpress-e2e/aliexpress-e2e/Framework/PageObjects/SearchPage.cs
+++ aliexpress-e2e/aliexpress-e2e/Framework/PageObjects/SearchPage.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using aliexpress_e2e.Framework.Utilities;
 
 namespace aliexpress_e2e.Framework.PageObjects
 {
-    class SearchPage
+    public class SearchPage
     {
+        //Driver
+        IWebDriver driver;
+        //Locators   
+        By nextPageButton = By.XPath("//button[@aria-label='Next page, current page 1']");
+        By productSelector = By.XPath("//div[@product-index='1']//a[@class='item-title']");
+
+        public SearchPage(IWebDriver driver) => this.driver = driver;
+
+        public void NavigateToSecondPage()
+        {
+            try
+            {
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("window.scrollBy(0,13000)");
+                driver.FindElement(nextPageButton).Click();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Could not click next page due to not scrolling or button not clickable " + e);
+            }
+            
+        }
+
+        public void ClickProduct()
+        {
+            WaitUtilities.WaitForPageToLoad(productSelector);
+            try
+            {
+                driver.FindElement(productSelector).Click();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Could not click product link verify stacktrace: " + e);
+            }
+            
+        }
     }
 }
