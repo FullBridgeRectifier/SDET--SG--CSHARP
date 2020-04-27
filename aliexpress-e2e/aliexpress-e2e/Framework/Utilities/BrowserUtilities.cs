e518d6200ae9d106414ba2fcd5dfd56c4be12610
Victor Moran <VMoran@dealersocket.com>
2020-04-27 10:49:32 -0500

    added browser utilities class

---
+++ aliexpress-e2e/aliexpress-e2e/Framework/Utilities/BrowserUtilities.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aliexpress_e2e.Framework.Utilities
+{
+    public class BrowserUtilities
+    {
+
+        static IWebDriver driver;
+        public BrowserUtilities(IWebDriver Wdriver) => driver = Wdriver;
+
+        public static void SwitchToNewestTab()
+        {
+            var handles = driver.WindowHandles;
+            SwitchToWindowHandle(handles[handles.Count - 1]);
+        }
+
+        public static void SwitchToWindowHandle(string handle)
+        {
+            driver.SwitchTo().Window(handle);
+        }
+    }
+}
