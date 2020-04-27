53302c705c7a718f1fc2bbdf0c415410e2f87f23
Victor Moran <VMoran@dealersocket.com>
2020-04-27 00:56:22 -0500

    Added initial broweser setup and app.config

--- aliexpress-e2e/aliexpress-e2e/Framework/Base.cs
+++ aliexpress-e2e/aliexpress-e2e/Framework/Base.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace aliexpress_e2e.Framework
 {
     class Base
     {
+        IWebDriver driver;
+
+        [SetUp]
+        public void InitializeBrowser()
+        {
+            if (ConfigurationManager.AppSettings["Website"] == "CHROME")
+            {
+                driver = new ChromeDriver();
+            }
+            else if (ConfigurationManager.AppSettings["Website"] == "FIREFOX")
+            {
+                driver = new FirefoxDriver();
+            }else
+            {
+                Assert.Fail("Error on type of browser at config file (App.config)");
+            }
+
+
+        }
+
+
+
+
     }
 }
