e4df7a41b935dcc51041140101fafd1894f5e2a4
Victor Moran <VMoran@dealersocket.com>
2020-04-27 00:35:38 -0500

    Adding first preliminar version of the framework

---
+++ aliexpress-e2e/aliexpress-e2e/Framework/Base.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aliexpress_e2e.Framework
+{
+    class Base
+    {
+    }
+}
---
+++ aliexpress-e2e/aliexpress-e2e/Framework/PageObjects/ProductPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aliexpress_e2e.Framework.PageObjects
+{
+    class ProductPage
+    {
+    }
+}
