ced19310969eb068ae5006bd98b689c272326cd8
Victor Moran <VMoran@dealersocket.com>
2020-04-27 10:40:20 -0500

    added product page methods

--- aliexpress-e2e/aliexpress-e2e/Framework/PageObjects/ProductPage.cs
+++ aliexpress-e2e/aliexpress-e2e/Framework/PageObjects/ProductPage.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace aliexpress_e2e.Framework.PageObjects
 {
-    class ProductPage
+    public class ProductPage
     {
+        IWebDriver driver;
+        String strQty;
+        By qtyInStock = By.XPath("//div[@class='product-quantity-tip']");
+
+        public ProductPage(IWebDriver driver) => this.driver = driver;
+
+        public void ValidateStock()
+        {
+            strQty = driver.FindElement(qtyInStock).Text;
+            strQty = strQty.Substring(0, 5);
+            strQty = strQty.Replace("\\D+", "");
+            if(int.Parse(strQty) > 0)
+            {
+                Console.WriteLine("Item has stock, this concludes the test");
+            }
+            else
+            {
+                Assert.Fail("Product is not in stock");
+            }
+        }
     }
 }
